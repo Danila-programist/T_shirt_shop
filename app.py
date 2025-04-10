efc7e72196522f381babe29ad5d87d5c8cfb16fa
Danila <danila25golubev@gmail.com>
2025-04-10 18:43:29 +0300

    add changes html add images add styles

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+products = [
+    {'id': 1, 'name': 'Горы на закате', 'price': 1200, 'image': '/static/poster1.jpg', 'description': 'Атмосферный пейзаж гор при закате.'},
+    {'id': 2, 'name': 'Минималистичный лес', 'price': 950, 'image': '/static/poster2.jpg', 'description': 'Простой и стильный лесной постер.'},
+    {'id': 3, 'name': 'Город в дождь', 'price': 1350, 'image': '/static/poster3.jpg', 'description': 'Городская романтика в каплях дождя.'}
+]
+
 @app.route('/')
 def index():
     return 'Hello World!'
