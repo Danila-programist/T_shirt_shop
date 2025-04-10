ff30cc00b1a2462d615bd0bd07bbe4ce9177146d
Danila <danila25golubev@gmail.com>
2025-04-10 18:59:59 +0300

    Добавление начальной главной страницы с товарами

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 
 
+products = [
+    {'id': 1, 'name': 'Футболка с капибарой', 'price': 999, 'image': '/static/images/capybara.jpg', 'description': 'Футболка с капибарой'},
+    {'id': 2, 'name': 'Футболка с дачей', 'price': 1269, 'image': 'static/images/dacha_troops.jpg', 'description': 'Футболка с дачей'},
+    {'id': 3, 'name': 'Футболка с Гомером', 'price': 1999, 'image': 'static/images/homer_simpson.jpg', 'description': 'Футболка с Гомером'},
+    {'id': 3, 'name': 'Футболка Ольга босс', 'price': 1699, 'image': 'static/images/olga_boss.jpg', 'description': 'Футболка Ольга босс'}
+]
+
 app = Flask(__name__)
 
-products = [
-    {'id': 1, 'name': 'Горы на закате', 'price': 1200, 'image': '/static/poster1.jpg', 'description': 'Атмосферный пейзаж гор при закате.'},
-    {'id': 2, 'name': 'Минималистичный лес', 'price': 950, 'image': '/static/poster2.jpg', 'description': 'Простой и стильный лесной постер.'},
-    {'id': 3, 'name': 'Город в дождь', 'price': 1350, 'image': '/static/poster3.jpg', 'description': 'Городская романтика в каплях дождя.'}
-]
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
